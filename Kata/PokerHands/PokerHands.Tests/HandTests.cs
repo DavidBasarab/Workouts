5c83f743838db4f99d485bb97fd2ca6529fb2c22
David Basarab <dbasarab617@gmail.com>
2015-03-02 22:04:16 -0500

    First Tests

---
+++ Kata/PokerHands/PokerHands.Tests/HandTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace PokerHands.Tests
+{
+    [TestFixture]
+    [Category("PokerHands.Tests")]
+    public class HandTests
+    {
+        [Test]
+        public void HandWithHighestRankWins()
+        {
+            var handRanker = new HandRanker();
+
+            var hand1 = new Card[]
+                        {
+                                new Card(Suit.Club, CardValue.Two), 
+                                new Card(Suit.Club, CardValue.Three), 
+                                new Card(Suit.Club, CardValue.Four), 
+                                new Card(Suit.Club, CardValue.Six), 
+                                new Card(Suit.Heart, CardValue.Seven), 
+                        };
+
+            var hand2 = new Card[]
+                        {
+                                new Card(Suit.Club, CardValue.Two), 
+                                new Card(Suit.Club, CardValue.Four), 
+                                new Card(Suit.Club, CardValue.Five), 
+                                new Card(Suit.Club, CardValue.Six), 
+                                new Card(Suit.Heart, CardValue.Eight), 
+                        };
+
+            var winner = handRanker.RankHands(hand1, hand2);
+
+            winner.Should().Be(2);
+        }
+    }
+}
---
+++ Kata/PokerHands/PokerHands/HandRanker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PokerHands
+{
+    public class HandRanker
+    {
+        public int RankHands(Card[] hand1, Card[] hand2)
+        {
+            return 2;
+        }
+    }
+}
