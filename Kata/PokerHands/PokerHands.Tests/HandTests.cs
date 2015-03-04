d5f90475c5f5eeb7060b175c240857aacc9dea94
David Basarab <dbasarab617@gmail.com>
2015-03-04 10:49:08 -0500

    Got basic Pair stuff working

--- Kata/PokerHands/PokerHands.Tests/HandTests.cs
+++ Kata/PokerHands/PokerHands.Tests/HandTests.cs
@@ -7,32 +7,283 @@ namespace PokerHands.Tests
     [Category("PokerHands.Tests")]
     public class HandTests
     {
-        [Test]
-        public void HandWithHighestRankWins()
+        private HandRanker _handRanker;
+
+        [SetUp]
+        public void SetUp()
         {
-            var handRanker = new HandRanker();
+            _handRanker = new HandRanker();
+        }
 
-            var hand1 = new Card[]
+        [Test]
+        public void Hand1WithHighestPairWinsPairOfTwos()
+        {
+            var hand1 = new[]
                         {
-                                new Card(Suit.Club, CardValue.Two), 
-                                new Card(Suit.Club, CardValue.Three), 
-                                new Card(Suit.Club, CardValue.Four), 
-                                new Card(Suit.Club, CardValue.Six), 
-                                new Card(Suit.Heart, CardValue.Seven), 
+                                new Card(Suit.Club, CardValue.Two),
+                                new Card(Suit.Diamond, CardValue.Two),
+                                new Card(Suit.Club, CardValue.Ten),
+                                new Card(Suit.Club, CardValue.Six),
+                                new Card(Suit.Heart, CardValue.Nine)
                         };
 
-            var hand2 = new Card[]
+            var hand2 = new[]
                         {
-                                new Card(Suit.Club, CardValue.Two), 
-                                new Card(Suit.Club, CardValue.Four), 
-                                new Card(Suit.Club, CardValue.Five), 
-                                new Card(Suit.Club, CardValue.Six), 
-                                new Card(Suit.Heart, CardValue.Eight), 
+                                new Card(Suit.Club, CardValue.Two),
+                                new Card(Suit.Club, CardValue.Four),
+                                new Card(Suit.Club, CardValue.Three),
+                                new Card(Suit.Club, CardValue.Six),
+                                new Card(Suit.Heart, CardValue.Ace)
                         };
 
-            var winner = handRanker.RankHands(hand1, hand2);
+            RankHands(hand1, hand2, 1);
+        }
 
-            winner.Should().Be(2);
+        [Test]
+        public void Hand2WithHighestPairWinsPairOfThrees()
+        {
+            var hand1 = new[]
+                        {
+                                new Card(Suit.Heart, CardValue.Two),
+                                new Card(Suit.Diamond, CardValue.Three),
+                                new Card(Suit.Club, CardValue.Three),
+                                new Card(Suit.Club, CardValue.Six),
+                                new Card(Suit.Heart, CardValue.Ace)
+                        };
+
+            var hand2 = new[]
+                        {
+                                new Card(Suit.Club, CardValue.Two),
+                                new Card(Suit.Diamond, CardValue.Two),
+                                new Card(Suit.Club, CardValue.Ten),
+                                new Card(Suit.Club, CardValue.Six),
+                                new Card(Suit.Heart, CardValue.Nine)
+                        };
+
+            RankHands(hand1, hand2, 1);
+        }
+
+        [Test]
+        public void Hand2WithHighestPairWinsPairOfFours()
+        {
+            var hand1 = new[]
+                        {
+                                new Card(Suit.Heart, CardValue.Two),
+                                new Card(Suit.Diamond, CardValue.Three),
+                                new Card(Suit.Club, CardValue.Three),
+                                new Card(Suit.Club, CardValue.Six),
+                                new Card(Suit.Heart, CardValue.Ace)
+                        };
+
+            var hand2 = new[]
+                        {
+                                new Card(Suit.Club, CardValue.Ten),
+                                new Card(Suit.Club, CardValue.Four),
+                                new Card(Suit.Club, CardValue.Two),
+                                new Card(Suit.Heart, CardValue.Nine),
+                                new Card(Suit.Diamond, CardValue.Four)
+                        };
+
+            RankHands(hand1, hand2, 2);
+        }
+
+        [Test]
+        public void Hand2WithHighestPairWinsPairOfTwos()
+        {
+            var hand1 = new[]
+                        {
+                                new Card(Suit.Heart, CardValue.Two),
+                                new Card(Suit.Club, CardValue.Four),
+                                new Card(Suit.Club, CardValue.Three),
+                                new Card(Suit.Club, CardValue.Six),
+                                new Card(Suit.Heart, CardValue.Ace)
+                        };
+
+            var hand2 = new[]
+                        {
+                                new Card(Suit.Club, CardValue.Two),
+                                new Card(Suit.Diamond, CardValue.Two),
+                                new Card(Suit.Club, CardValue.Ten),
+                                new Card(Suit.Club, CardValue.Six),
+                                new Card(Suit.Heart, CardValue.Nine)
+                        };
+
+            RankHands(hand1, hand2, 2);
+        }
+
+        [Test]
+        public void HandWithHighestRankWinsAceHigh()
+        {
+            var hand1 = new[]
+                        {
+                                new Card(Suit.Club, CardValue.Two),
+                                new Card(Suit.Club, CardValue.Three),
+                                new Card(Suit.Club, CardValue.Four),
+                                new Card(Suit.Club, CardValue.Queen),
+                                new Card(Suit.Heart, CardValue.Nine)
+                        };
+
+            var hand2 = new[]
+                        {
+                                new Card(Suit.Club, CardValue.King),
+                                new Card(Suit.Club, CardValue.Four),
+                                new Card(Suit.Club, CardValue.Jack),
+                                new Card(Suit.Club, CardValue.Ace),
+                                new Card(Suit.Heart, CardValue.Eight)
+                        };
+
+            RankHands(hand1, hand2, 2);
+        }
+
+        [Test]
+        public void HandWithHighestRankWinsEightHigh()
+        {
+            var hand1 = new[]
+                        {
+                                new Card(Suit.Club, CardValue.Two),
+                                new Card(Suit.Club, CardValue.Three),
+                                new Card(Suit.Club, CardValue.Four),
+                                new Card(Suit.Club, CardValue.Six),
+                                new Card(Suit.Heart, CardValue.Seven)
+                        };
+
+            var hand2 = new[]
+                        {
+                                new Card(Suit.Club, CardValue.Two),
+                                new Card(Suit.Club, CardValue.Four),
+                                new Card(Suit.Club, CardValue.Five),
+                                new Card(Suit.Club, CardValue.Six),
+                                new Card(Suit.Heart, CardValue.Eight)
+                        };
+
+            RankHands(hand1, hand2, 2);
+        }
+
+        [Test]
+        public void HandWithHighestRankWinsJackHigh()
+        {
+            var hand1 = new[]
+                        {
+                                new Card(Suit.Club, CardValue.Two),
+                                new Card(Suit.Club, CardValue.Three),
+                                new Card(Suit.Club, CardValue.Four),
+                                new Card(Suit.Club, CardValue.Six),
+                                new Card(Suit.Heart, CardValue.Nine)
+                        };
+
+            var hand2 = new[]
+                        {
+                                new Card(Suit.Club, CardValue.Two),
+                                new Card(Suit.Club, CardValue.Four),
+                                new Card(Suit.Club, CardValue.Jack),
+                                new Card(Suit.Club, CardValue.Six),
+                                new Card(Suit.Heart, CardValue.Eight)
+                        };
+
+            RankHands(hand1, hand2, 2);
+        }
+
+        [Test]
+        public void HandWithHighestRankWinsKingHigh()
+        {
+            var hand1 = new[]
+                        {
+                                new Card(Suit.Club, CardValue.Two),
+                                new Card(Suit.Club, CardValue.Three),
+                                new Card(Suit.Club, CardValue.Four),
+                                new Card(Suit.Club, CardValue.Queen),
+                                new Card(Suit.Heart, CardValue.Nine)
+                        };
+
+            var hand2 = new[]
+                        {
+                                new Card(Suit.Club, CardValue.King),
+                                new Card(Suit.Club, CardValue.Four),
+                                new Card(Suit.Club, CardValue.Jack),
+                                new Card(Suit.Club, CardValue.Six),
+                                new Card(Suit.Heart, CardValue.Eight)
+                        };
+
+            RankHands(hand1, hand2, 2);
+        }
+
+        [Test]
+        public void HandWithHighestRankWinsNineHigh()
+        {
+            var hand1 = new[]
+                        {
+                                new Card(Suit.Club, CardValue.Two),
+                                new Card(Suit.Club, CardValue.Three),
+                                new Card(Suit.Club, CardValue.Four),
+                                new Card(Suit.Club, CardValue.Six),
+                                new Card(Suit.Heart, CardValue.Nine)
+                        };
+
+            var hand2 = new[]
+                        {
+                                new Card(Suit.Club, CardValue.Two),
+                                new Card(Suit.Club, CardValue.Four),
+                                new Card(Suit.Club, CardValue.Five),
+                                new Card(Suit.Club, CardValue.Six),
+                                new Card(Suit.Heart, CardValue.Eight)
+                        };
+
+            RankHands(hand1, hand2, 1);
+        }
+
+        [Test]
+        public void HandWithHighestRankWinsQueenHigh()
+        {
+            var hand1 = new[]
+                        {
+                                new Card(Suit.Club, CardValue.Two),
+                                new Card(Suit.Club, CardValue.Three),
+                                new Card(Suit.Club, CardValue.Four),
+                                new Card(Suit.Club, CardValue.Queen),
+                                new Card(Suit.Heart, CardValue.Nine)
+                        };
+
+            var hand2 = new[]
+                        {
+                                new Card(Suit.Club, CardValue.Two),
+                                new Card(Suit.Club, CardValue.Four),
+                                new Card(Suit.Club, CardValue.Jack),
+                                new Card(Suit.Club, CardValue.Six),
+                                new Card(Suit.Heart, CardValue.Eight)
+                        };
+
+            RankHands(hand1, hand2, 1);
+        }
+
+        [Test]
+        public void HandWithHighestRankWinsTenHigh()
+        {
+            var hand1 = new[]
+                        {
+                                new Card(Suit.Club, CardValue.Two),
+                                new Card(Suit.Club, CardValue.Three),
+                                new Card(Suit.Club, CardValue.Ten),
+                                new Card(Suit.Club, CardValue.Six),
+                                new Card(Suit.Heart, CardValue.Nine)
+                        };
+
+            var hand2 = new[]
+                        {
+                                new Card(Suit.Club, CardValue.Two),
+                                new Card(Suit.Club, CardValue.Four),
+                                new Card(Suit.Club, CardValue.Three),
+                                new Card(Suit.Club, CardValue.Six),
+                                new Card(Suit.Heart, CardValue.Eight)
+                        };
+
+            RankHands(hand1, hand2, 1);
+        }
+
+        private void RankHands(Card[] hand1, Card[] hand2, int expectedWinner)
+        {
+            var winner = _handRanker.RankHands(hand1, hand2);
+
+            winner.Should().Be(expectedWinner);
         }
     }
 }
--- Kata/PokerHands/PokerHands/HandRanker.cs
+++ Kata/PokerHands/PokerHands/HandRanker.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Linq;
 
 namespace PokerHands
 {
@@ -6,6 +6,35 @@ namespace PokerHands
     {
         public int RankHands(Card[] hand1, Card[] hand2)
         {
+            var hand1ByValues = hand1.GroupBy(i => i.CardValue).Select(g => new
+                                                                            {
+                                                                                    Value = g.Key,
+                                                                                    Count = g.Select(v => (int)v.CardValue).Count()
+                                                                            }).ToList();
+
+            var hand2ByValues = hand2.GroupBy(i => i.CardValue).Select(g => new
+                                                                            {
+                                                                                    Value = g.Key,
+                                                                                    Count = g.Select(v => (int)v.CardValue).Count()
+                                                                            }).ToList();
+
+            // One of the hands have a pair
+            if (hand1ByValues.Any(i => i.Count == 2) || hand2ByValues.Any(i => i.Count == 2))
+            {
+                if (hand1ByValues.Any(i => i.Count == 2) && hand2ByValues.All(i => i.Count != 2)) return 1;
+
+                if (hand1ByValues.All(i => i.Count != 2) && hand2ByValues.Any(i => i.Count == 2)) return 2;
+
+                var hand1PairValue = hand1ByValues.Where(i => i.Count == 2).Select(i => i.Value).FirstOrDefault();
+                var hand2PairValue = hand2ByValues.Where(i => i.Count == 2).Select(i => i.Value).FirstOrDefault();
+
+                if (hand1PairValue > hand2PairValue) return 1;
+
+                return 2;
+            }
+
+            if (hand1.Max(i => (int)i.CardValue) >= hand2.Max(i => (int)i.CardValue)) return 1;
+
             return 2;
         }
     }
