2bc58f8df41532e8df578c9dda8a252f666269a4
David Basarab <dbasarab617@gmail.com>
2018-07-01 00:13:08 -0400

    Added problem 9 structure

---
+++ Euler/Problem9.js
@@ -0,0 +1,19 @@
+/*
+     Problem from Site
+
+     A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+        a^2 + b^2 = c^2
+        For example, 3^2 + 4^2 = 9 + 16 = 25 = 52.
+
+        There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+        Find the product abc.
+*/
+
+var common = require("./common");
+
+module.exports = {
+    solveProblem: function () {
+        console.log("Go and get some sleep");
+    }
+}.solveProblem();
