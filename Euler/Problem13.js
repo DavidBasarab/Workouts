5bdac1ae09f34b31a7ff86fe2fb3bbc9312fc920
David Basarab <dbasarab617@gmail.com>
2018-07-06 23:31:34 -0400

    created js file

---
+++ Euler/Problem13.js
@@ -0,0 +1,13 @@
+/*
+     Problem from Site
+
+*/
+
+var common = require("./common");
+
+module.exports = {
+    solveProblem: async function () {
+        console.log(`Going to solve 13 . . . .`);
+
+    }
+}
