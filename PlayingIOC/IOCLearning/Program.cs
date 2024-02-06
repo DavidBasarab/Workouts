98446f008aa9d787a04871ef7f1015332a09f052
DavidBasarab <dbasarab@haivision.com>
2024-02-06 16:56:49 -0500

    playing with background service

--- PlayingIOC/IOCLearning/Program.cs
+++ PlayingIOC/IOCLearning/Program.cs
@@ -1,17 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using IOCLearning;
+﻿using IOCLearning;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-Console.WriteLine("Hello, World!");
-
-
-HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.AddHostedService<Worker>();
 builder.Services.AddSingleton<IMessageWriter, MessageWriter>();
 
-using IHost host = builder.Build();
+using var host = builder.Build();
 
 host.Run();
--- PlayingIOC/IOCLearning/Worker.cs
+++ PlayingIOC/IOCLearning/Worker.cs
@@ -4,6 +4,20 @@ namespace IOCLearning;
 
 public sealed class Worker(IMessageWriter messageWriter) : BackgroundService
 {
+	public override Task StartAsync(CancellationToken cancellationToken)
+	{
+		messageWriter.Write("Starting worker");
+
+		return base.StartAsync(cancellationToken);
+	}
+
+	public override Task StopAsync(CancellationToken cancellationToken)
+	{
+		messageWriter.Write("Stopping worker");
+
+		return base.StopAsync(cancellationToken);
+	}
+
 	protected override async Task ExecuteAsync(CancellationToken stoppingToken)
 	{
 		while (!stoppingToken.IsCancellationRequested)
